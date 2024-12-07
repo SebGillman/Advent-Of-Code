53a9f6d7787c73758032d7381e937019cdd1fbe0
SebGillman <sebinred1@gmail.com>
2024-12-07 09:02:59 +0000

    2024 task 7.2

---
+++ 2024/7/two.ts
@@ -0,0 +1,34 @@
+import * as fs from "fs/promises";
+import test from "node:test";
+
+const file = await fs.readFile("./2024/7/input.txt", { encoding: "utf-8" });
+
+const lines = file.split("\r\n");
+
+let res = 0;
+
+let numbers: Array<number> = [];
+let testVal: number;
+
+function possibleOperators(curr: number, index: number): boolean {
+  // console.log(curr, numbers[index], parseInt(`${curr}${numbers[index]}`));
+  if (curr > testVal) return false;
+  if (index == numbers.length) return curr == testVal;
+
+  return (
+    possibleOperators(curr + numbers[index], index + 1) ||
+    possibleOperators(curr * numbers[index], index + 1) ||
+    possibleOperators(parseInt(`${curr}${numbers[index]}`), index + 1)
+  );
+}
+
+for (const line of lines) {
+  const [testValString, numString] = line.split(": ");
+  testVal = parseInt(testValString);
+  numbers = numString.split(" ").map((num) => parseInt(num));
+
+  //   console.log(testVal, numbers);
+  if (possibleOperators(numbers[0], 1)) res += testVal;
+}
+
+console.log(res);
